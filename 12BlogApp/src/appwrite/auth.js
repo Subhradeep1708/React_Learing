d1d2fc8f503dc4aa396d4ae50f32fd60f89212ea
Subhradeep1708 <subhradeep1708@gmail.com>
2025-03-27 00:18:27 +0530

    Create Auth service

--- 12BlogApp/src/appwrite/auth.js
+++ 12BlogApp/src/appwrite/auth.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-catch */
 import conf from '../conf/conf.js'
 import { Client, Account, ID } from 'appwrite';
 
@@ -13,7 +14,46 @@ export class AuthService {
         this.account = new Account(this.client);
     }
 
-    async
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                //call another method
+                return this.login({ email, password });//if account created successfully then login directly
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({ email, password }) {
+        try {
+            return await this.account.createEmailPasswordSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    // to check if user is logged in or not
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite Error:: getCurrentUser Error ", error);
+        }
+        return null; // if no user is logged in then return null
+    }
+
+    async logout() {
+        try {
+            return await this.account.deleteSessions(); // delete all sessions
+        } catch (error) {
+            console.log("Appwrite Error:: logout Error ", error);
+        }
+    }
+
 }
 
 const authService = new AuthService(); // this is an object of AuthService class just add .func_name and use anywhere
